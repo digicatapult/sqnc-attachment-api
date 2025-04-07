4d43054c4e9e586956c433824926f7421192f56d
Ellenn-A <58051865+Ellenn-A@users.noreply.github.com>
2025-04-07 16:24:09 +0100

    fetch attachments by hash or id (#36)

--- src/models/attachment.ts
+++ src/models/attachment.ts
@@ -26,3 +26,24 @@ export const internalAttachmentCreateBodyParser = z.object({
   ownerAddress: z.string(),
 })
 export type InternalAttachmentCreateBody = z.infer<typeof internalAttachmentCreateBodyParser>
+
+export const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+/**
+ * UUID format identifier
+ * @example 282439cd-ebc8-420e-bfbb-0de8fb3149ed
+ * @pattern ^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$
+ */
+export type AttachmentId = string
+
+/**
+ * File integrity hash
+ * @example QmX5g1GwdB87mDoBTpTgfuWD2VKk8SpMj5WMFFGhhFacHN
+ * @pattern ^[a-zA-Z0-9]+$
+ */
+export type AttachmentHash = string
+
+/**
+ * Either an attachment UUID or an file integrity hash
+ */
+export type AttachmentIdOrHash = AttachmentId | AttachmentHash
